7eecfea0661b1e956b9cf3a3275f0985738e79f0
VadimGera <vadimnvidia@gmail.com>
2023-03-18 17:14:52 +0300

    Add Progress Bar png

--- Assets/Scripts/ButtonScript/ButtonDown.cs
+++ Assets/Scripts/ButtonScript/ButtonDown.cs
@@ -7,9 +7,11 @@ using UnityEngine.SceneManagement;
 public class ButtonDown : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField] private string sceneName;
+    [SerializeField] private GameObject loadingScreen;
     
     public void OnPointerDown(PointerEventData eventData)
     {
+        loadingScreen.SetActive(true);
         SceneManager.LoadSceneAsync(sceneName);
     }
 }
