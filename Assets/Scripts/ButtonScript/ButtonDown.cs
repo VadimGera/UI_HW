4100c9e94b2ac3641c0a4a42bfacd8d0d5a42859
VadimGera <vadimnvidia@gmail.com>
2023-03-19 15:39:52 +0300

    Add Loadmanager

--- Assets/Scripts/ButtonScript/ButtonDown.cs
+++ Assets/Scripts/ButtonScript/ButtonDown.cs
@@ -1,13 +1,17 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace ButtonScript
 {
     public class ButtonDown : MonoBehaviour, IPointerDownHandler
     {
+        [SerializeField] private Slider _slider;
         [SerializeField] private string sceneName;
         [SerializeField] private GameObject loadingScreen;
+        [SerializeField] private TextMeshProUGUI _progressPercent;
     
         public void OnPointerDown(PointerEventData eventData)
         {
---
+++ Assets/Scripts/LoadProgress/LoadManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace LoadProgress
+{
+    public class LoadManager : MonoBehaviour, IPointerDownHandler
+    {
+        [SerializeField] private Slider _slider;
+        [SerializeField] private string sceneName;
+        [SerializeField] private GameObject loadingScreen;
+        [SerializeField] private TextMeshProUGUI _progressPercent;
+    
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            
+            SceneManager.LoadSceneAsync(sceneName);
+
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+            loadingScreen.SetActive(true);
+
+            while (!operation.isDone)
+            {
+                float progress = Mathf.Clamp01(operation.progress / 0.8f);
+
+                _slider.value = progress;
+                _progressPercent.text = Mathf.Round(progress * 100) + "%";
+                
+            }
+        }
+
+        
+    }
+}
