8a695946e7877b18c4defdaaad2e6851a9ceb55c
VadimGera <vadimnvidia@gmail.com>
2023-03-19 03:05:27 +0300

    Проблемки с прцоентами

--- Assets/Scripts/ButtonScript/ButtonDown.cs
+++ Assets/Scripts/ButtonScript/ButtonDown.cs
@@ -1,17 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class ButtonDown : MonoBehaviour, IPointerDownHandler
+namespace ButtonScript
 {
-    [SerializeField] private string sceneName;
-    [SerializeField] private GameObject loadingScreen;
-    
-    public void OnPointerDown(PointerEventData eventData)
+    public class ButtonDown : MonoBehaviour, IPointerDownHandler
     {
-        loadingScreen.SetActive(true);
-        SceneManager.LoadSceneAsync(sceneName);
+        [SerializeField] private string sceneName;
+        [SerializeField] private GameObject loadingScreen;
+    
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            loadingScreen.SetActive(true);
+            SceneManager.LoadSceneAsync(sceneName);
+        }
     }
 }
